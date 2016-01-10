12e00879ee8161ba3cbf50281201c89c29b223ba
Viacheslav Wolf <vwo@netlands.de>
2016-01-10 20:04:55 +0100

    Parser for "http://audioknigi.club/" updatet for ajax-Usage

--- extension/parsers/audioknigi.club.js
+++ extension/parsers/audioknigi.club.js
@@ -1,35 +1,26 @@
-function getPlaylist(){
+function parse(){
+
     var playList = {};
-    var $scripts = $('script');
-    var mp3Urls = [];
-    $scripts.each(function(){
-        var script  = $(this).text();
-        if(script.indexOf("new jPlayerPlaylist") > -1){
-            mp3Urls = script.match(/(http.*\.mp3)/g);
+    $.ajax('http://audioknigi.club/rest/bid/34088', {
+        async: false,
+        success: function(content){
+            playList = content;
         }
     });
 
-    $.each(mp3Urls, function(){
-        var url = this + "";
-        playList[url] = {
+    var mp3 = {};
+    $.each(playList, function(key, value){
+        var url = value.mp3;
+        mp3[url] = {
             url: url,
-            titel: url.match(/,\/(.*)/g)[0].replace(',/', '')
+            titel: value.title
         };
     });
-    return playList;
-}
-
-function parse(){
-
-    var playlist = getPlaylist();
-    if(playlist.length == 0){
-        return false;
-    }
 
     return {
         titel: $('.topic-header h1').text().trim(),
         desc: $('.topic-content').text().trim(),
         image: $('.topic img:last').attr('src'),
-        playlist: playlist
+        playlist: mp3
     };
 }
