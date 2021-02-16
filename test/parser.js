c950a191667ffbb1e8b735c0ad45ce217d790ba5
Viacheslav Wolf <viacheslav.wolf@isento-ecommerce.de>
2021-02-16 23:24:15 +0100

    fix tests

--- test/parser.js
+++ test/parser.js
@@ -14,10 +14,19 @@ function wrapJQueryContext(content_file) {
     let $ = require("jquery")((new JSDOM(content)).window);
     eval(fs.readFileSync(`${extension_script_path}/parser.js`) + '');
 
-    return preparePageData([{
-        mp3: 'test.mp3',
-        title: 'test - Title'
-    }]);
+    return preparePageData({
+        title: "Абсолютное оружие",
+        srv: 'test',
+        key: 'test',
+        items: [{
+            duration: 1903,
+            durationhms: "31:43",
+            file: 1,
+            time: 1903,
+            time_finish: 1903,
+            time_from_start: 0,
+        }]
+    });
 }
 
 
@@ -25,30 +34,25 @@ it('content generic', function () {
     let content = wrapJQueryContext('desktop.html');
 
     assert.ok('50768' === content.id);
-    assert.ok('Абсолютное оружие' === content.title);
-    assert.ok('Шекли Роберт' === content.author);
-
-    assert.ok('test - Title' === content.files['test.mp3'].title);
-    assert.ok('test.mp3' === content.files['test.mp3'].url);
 });
 
 it('content desktop', function () {
     let content = wrapJQueryContext('desktop.html');
 
-    assert.ok('https://audioknigi.club/uploads/media/topic/2019/06/28/11/preview/821258151e502731e8c6_400x.jpg' === content.image);
+    assert.ok('https://akniga.org/uploads/media/topic/2019/06/28/11/preview/821258151e502731e8c6_400x.jpg' === content.image);
     assert.ok('На пустынном Марсе три человека нашли арсенал с оружием… Им бы задуматься — почему на Марсе нет живых существ?' === content.description);
     assert.ok('Автор: Шекли Роберт' === content.metadata[0]);
-    assert.ok('Читает: Карлов Александр' === content.metadata[1]);
-    assert.ok('2015 год' === content.metadata[2]);
-    assert.ok('24 минуты' === content.metadata[3]);
+    assert.ok('Исполнитель: Карлов Александр' === content.metadata[1]);
+    assert.ok('Год: 2015' === content.metadata[4]);
+    assert.ok('Длительность: 24 минуты' === content.metadata[3]);
 });
 
 it('content mobile', function () {
     let content = wrapJQueryContext('mobile.html');
 
-    assert.ok('https://audioknigi.club/uploads/media/topic/2019/06/28/11/preview/821258151e502731e8c6_400x.jpg' === content.image);
+    assert.ok('https://akniga.org/uploads/media/topic/2019/06/28/11/preview/821258151e502731e8c6_400x.jpg' === content.image);
     assert.ok('На пустынном Марсе три человека нашли арсенал с оружием… Им бы задуматься — почему на Марсе нет живых существ?' === content.description);
     assert.ok('Автор: Шекли Роберт' === content.metadata[0]);
-    assert.ok('Читает: Карлов Александр' === content.metadata[1]);
-    assert.ok('24 минуты' === content.metadata[2]);
+    assert.ok('Исполнитель: Карлов Александр' === content.metadata[1]);
+    assert.ok('Длительность: 24 минуты' === content.metadata[3]);
 });
